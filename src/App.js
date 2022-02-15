d37f4ab2674845d6ce876458a43e6b32f01fda5f
Sanchit Hajela <sanchit10hajela@gmail.com>
2022-02-15 11:59:45 +0530

    added input box and added CSS for search bar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Real Time Search</h1>
+      <Search />
+      <div className="main-content">
+        search results .....
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div className="search">
+      <input type="text" placeholder="Search" className="search__input" />
+    </div>
+  );
+};
+export default Search;
